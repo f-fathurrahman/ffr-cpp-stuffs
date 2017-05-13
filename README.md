32d63e6858a3ead8920cbed8a206f1ff998bfa1e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-05-13 22:03:41 +0700

    Initial commit: test auto

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+clang++ -std=c++11 filename.cpp
+g++ -std=c++11 filename.cpp
+icpc -std=c++11 filename.cpp
+pgc++ -std=c++11 filename.cpp
+```
---
+++ test_auto.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  auto x = 3.2;
+  cout << "x = " << x << endl;
+  
+  auto f = true;
+  cout << "f = " << f << endl;
+  
+  auto g = 0x111;
+  cout << "g = " << g << endl;
+
+  auto b = 0b0101;
+  cout << "b = " << b << endl;
+
+  auto str1 = "Fadjar Fathurrahman";
+  cout << "str1 = " << str1 << endl;
+
+  auto chr1 = 'a';
+  cout << "chr1 = " << chr1 << endl;
+
+  return 0;
+}
