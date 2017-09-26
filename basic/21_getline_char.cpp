8eb9168b927eb4942a7ee7eeac9fd639c083010f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-09-26 11:12:34 +0700

    Add 21

---
+++ basic/21_getline_char.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  const int N = 20;
+  char name[N];
+  char dessert[N];
+
+  cout << "Enter your name:\n";
+  cin.getline(name, N);
+
+  cout << "Enter your favorite dessert:\n";
+  cin.getline(dessert, N);
+
+  cout << "Hello " << name << endl;
+  cout << "Your favorite dessert is " << dessert << endl;
+
+  return 0;
+}
