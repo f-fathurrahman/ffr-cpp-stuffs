80cacecd8281bbd65f680c019fe0225a5735caed
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-10-30 14:04:36 +0700

    Modify Makefile

--- SDL_1.2/wayang_sandbox/build.sh
+++ SDL_1.2/wayang_sandbox/build.sh
@@ -3,6 +3,6 @@ basn=`basename $1 .cpp`
 
 rm -vf $basn.x
 
-g++ -I/usr/include/SDL -Wall $1 -o $basn.x libmain.a -lSDL -lSDL_mixer -lSDL_ttf -lSDL_image
+g++ -I/usr/include/SDL $1 -o $basn.x libmain.a -lSDL -lSDL_mixer -lSDL_ttf -lSDL_image
 
 echo "Executable: $basn.x"
