0d244c7d9073dcb1e11ae9dbac46145c3dd02f72
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-04-07 22:26:32 +0700

    Finally I figured out the libraries that need to be linked

--- opencv/v3_build.sh
+++ opencv/v3_build.sh
@@ -3,13 +3,20 @@ basn=`basename $1 .cpp`
 
 PRE=/home/efefer/mysoftwares/opencv-3.4.1
 PRELIB=$PRE/lib
+
 CORE=$PRELIB/libopencv_core.a
+IMGPROC=$PRELIB/libopencv_imgproc.a
+IMGCODECS=$PRELIB/libopencv_imgcodecs.a
+HIGHGUI=$PRELIB/libopencv_highgui.a
 
 ITT=$PRE/share/OpenCV/3rdparty/lib/libittnotify.a
+IPROTO=$PRE/share/OpenCV/3rdparty/lib/liblibprotobuf.a
 
 INC="-I$PRE/include"
 
-LIBS="$CORE $ITT -lblas -llapack -lpthread -lz -ldl"
+LIBS="$IMGCODECS $HIGHGUI $IMGPROC $CORE $ITT \
+-lblas -llapack -ltiff -ljpeg -lgtk-3 -lgdk-3 -lcairo -lgdk_pixbuf-2.0 \
+-lgobject-2.0 -lglib-2.0 -lpng12 -ljasper -lIlmImf -lwebp -lpthread -lz -ldl"
 
 rm -vf $basn.x
 
