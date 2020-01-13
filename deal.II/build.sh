1bfbbfa460d6448f337aca16104f287e4a30038c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-01-13 13:11:05 +0700

    Modify to work for v8.5.1

--- deal.II/build.sh
+++ deal.II/build.sh
@@ -2,5 +2,9 @@
 basn=`basename $1 .cpp`
 
 rm -vf $basn.x
-mpic++ -I/usr/include/trilinos -Wall $1 -o $basn.x -ldeal.ii
+
+# Tested for Ubuntu 18.04
+# Also include trilinos because it is not included in the INCLUDE path.
+mpic++ -std=c++11 -I/usr/include/trilinos -Wall $1 -o $basn.x -ldeal.ii
+
 echo "Executable: $basn.x"
--- deal.II/step01.cpp
+++ deal.II/step01.cpp
@@ -6,6 +6,8 @@
 // Some function to generate standard grids
 #include <deal.II/grid/grid_generator.h>
 
+#include <deal.II/grid/manifold_lib.h>
+
 // output of grids in various graphics formats
 #include <deal.II/grid/grid_out.h>
 
@@ -35,9 +37,52 @@ void create_first_grid()
     return;
 }
 
+
+void create_second_grid()
+{
+    Triangulation<2> triangulation;
+
+    const Point<2> center(1, 0);
+    const double inner_radius = 0.5, outer_radius = 1.0;
+
+    GridGenerator::hyper_shell(triangulation, center, inner_radius, outer_radius, 10);
+    
+    const SphericalManifold<2> manifold_description(center);
+    triangulation.set_manifold(0, manifold_description);
+    triangulation.set_all_manifold_ids(0);
+
+    for(unsigned int step = 0; step < 5; step++)
+    {
+        Triangulation<2>::active_cell_iterator cell = triangulation.begin_active();
+        Triangulation<2>::active_cell_iterator endc = triangulation.end();
+        for(; cell != endc; cell++)
+        {
+            for(unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; v++)
+            {
+                const double distance_from_center = center.distance(cell->vertex(v));
+                if(std::fabs(distance_from_center - inner_radius) < 1e-10)
+                {
+                    cell->set_refine_flag();
+                    break;
+                }
+            }
+        }
+        triangulation.execute_coarsening_and_refinement();
+    }
+
+    std::ofstream out("IMG_grid-2.eps");
+    GridOut grid_out;
+    grid_out.write_eps(triangulation, out);
+
+    std::cout << "Grid written to IMG_grid-2.eps" << std::endl;
+
+}
+
+
 int main()
 {
     create_first_grid();
+    create_second_grid();
 
     return 0;
 }
