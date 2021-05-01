db8ba72adccd4f57de528d499ba352e3fb4b812c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-05-01 09:19:22 +0700

    Update to Deal.II version 9.2.0, using CMake

--- deal.II/build_v9.sh
+++ deal.II/build_v9.sh
@@ -1,6 +1,15 @@
 prefix=`basename $1 .cpp`
 FLAGS="-std=c++1z -Wall"
-INCLUDE="-I/opt/dealii-9.1.1_gcc_openmpi/include"
-LIBS="-Wl,-rpath,/opt/dealii-9.1.1_gcc_openmpi/lib/ -L/opt/dealii-9.1.1_gcc_openmpi/lib/ -ldeal_II"
+
+DEALII_HOME="/opt/dealii-9.2.0_gcc_openmpi"
+
+INCLUDE="-I${DEALII_HOME}/include \
+-I/usr/include/trilinos
+"
+
+echo $INCLUDE
+
+LIBS="-Wl,-rpath,${DEALII_HOME}/lib/ -L${DEALII_HOME}/lib -ldeal_II \
+-ltrilinos_teuchoscomm -ltrilinos_teuchoscore"
 
 mpic++ $INCLUDE $FLAGS $1 -o $prefix.x $LIBS
