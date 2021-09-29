08e5731563ec70bcd3063497dcfcc8c41e6c82c8
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-09-29 20:45:27 +0700

    Input nama, penggunaan getline

---
+++ Praktikum2021/progs/input_nama_v1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string nama;
+  cout << "Siapa nama Anda? ";
+  cin >> nama;
+  cout << "Halo " << nama << "!" << endl;
+}
---
+++ Praktikum2021/progs/input_nama_v2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string nama;
+  cout << "Siapa nama Anda? ";
+  getline(cin, nama);
+  cout << "Halo " << nama << "!" << endl;
+}
