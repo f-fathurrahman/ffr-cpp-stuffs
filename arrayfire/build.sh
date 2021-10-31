4498b4551938c9ad46c39fc72b881c2fb92292bf
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-10-31 20:33:18 +0700

    Test ArrayFire, f64 CPU and OpenCL

---
+++ arrayfire/build.sh
@@ -0,0 +1,12 @@
+prefix=`basename $1 .cpp`
+FLAGS="-std=c++11 -Wall" # need std=c++11 ?
+
+ARRAYFIRE_HOME="/home/efefer/mysoftwares/arrayfire-3.7.3"
+
+INCLUDE="-I${ARRAYFIRE_HOME}/include "
+
+echo $INCLUDE
+
+LIBS="-Wl,-rpath,${ARRAYFIRE_HOME}/lib/ -L${ARRAYFIRE_HOME}/lib -lafcpu -laf"
+
+g++ $INCLUDE $FLAGS $1 -o $prefix.x $LIBS
